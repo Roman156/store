18c0226d0b4a7ab1adf4fa05c2e6fc294b87fa6b
Roman156 <97851825+Roman156@users.noreply.github.com>
2024-05-10 02:51:34 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper", {
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		dynamicBullets: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
